9a17d4cb37a5c9553f2019f28fdf4237eb9eb23e
Madhusudhan B <56335301+madhuyadu@users.noreply.github.com>
2022-09-09 07:00:26 +0000

    Added the Flask project: sample

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+#__name__ is already defined in any Python script
+print(__name__)
+app = Flask(__name__)  #create flask application
+
+
+@app.route("/")  #rootpage/homepage URL
+#returns below function
+# When root page is accessed, return "Hello, World"
+def hello_world():
+    return "Hello, World"
+
+
+if __name__ == "__main__":
+    print("I'm inside if now !!")
+    #'0.0.0.0' is local development server
+    app.run(host='0.0.0.0', debug=True)  #START the app
