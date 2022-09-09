7f267381158a59f3422f4bbeea1d23853149bfd6
Madhusudhan B <56335301+madhuyadu@users.noreply.github.com>
2022-09-09 11:35:53 +0000

    Added home3.html: rendering info dynamically inside home3.html from jobitem.html & also from app.py using %%operators (JOBS related & COMPANY related info)
    added jobitem.html: readymade info to be added/rendered inside for loop of home3.html

--- app.py
+++ app.py
@@ -3,13 +3,35 @@ from flask import Flask, render_template
 print(__name__)
 app = Flask(__name__)  #create flask application
 
+JOBS = [{
+    'id': 1,
+    'Title': 'Data Analyst',
+    'Location': 'Bengaluru, India',
+    'Salary': '10,00,000'
+}, {
+    'id': 2,
+    'Title': 'Data Scientist',
+    'Location': 'Delhi, India',
+    'Salary': '12,00,000'
+}, {
+    'id': 3,
+    'Title': 'Frontend Engineer',
+    'Location': 'Remote',
+    'Salary': '8,00,000'
+}, {
+    'id': 4,
+    'Title': 'Backend Engineer',
+    'Location': 'San Francisco, India',
+    'Salary': '$120,000'
+}]
+
 
 @app.route("/")  #rootpage/homepage URL
 #returns below function
 # When root page is accessed, return "Hello, World"
 def hello_world():
     #return "Hello, World"
-    return render_template("home2.html")
+    return render_template("home3.html", jobs=JOBS, company_name='Jovian')
 
 
 if __name__ == "__main__":
